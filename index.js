64bca8d2485bbc621cfc85ea7097fbb3ebebac42
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2025-02-22 19:00:22 +0600

    add cors

--- index.js
+++ index.js
@@ -21,6 +21,13 @@ cors({
   credentials: true,
 });
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(express.json());
 
 // MongoDB connection
