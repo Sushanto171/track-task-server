24f10ace29a7683ba6a1ba619193ef826c9bad3e
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2025-02-22 19:42:09 +0600

    add cors

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ const corsOptions = {
   credentials: true,
 };
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 // Apply CORS Middleware
 app.use(cors(corsOptions));
 
